00aada6adcd010d1faedae1fc000f3b448de7585
SuvorovAV <78296129+SuvorovAV@users.noreply.github.com>
2021-09-08 16:32:00 +0500

    Add files via upload

---
+++ myproject/urls.py
@@ -0,0 +1,33 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+# функция для возврата файла
+from myparser.views import home_page
+from myparser.views import home_page, WelderCreate
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Url и функция, которая вернет файл
+    path('', home_page),
+    path('welder/', WelderCreate.as_view())
+]
+
+# включаем возможность обработки файлов
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
